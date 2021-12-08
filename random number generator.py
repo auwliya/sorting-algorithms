22ee73b40a42011064980a902f6644be198288a1
Fungi 3 <wricie@gmail.com>
2021-12-08 09:47:21 +0100

    Update random number generator.py

--- random number generator.py
+++ random number generator.py
@@ -2,8 +2,8 @@ from random import seed
 from random import shuffle
 # seed random number generator
 seed(1)
-# prepare a sequence
-sequence = [i for i in range(100)]
-# randomly shuffle the sequence
-shuffle(sequence)
-print(sequence)
+# list from 0 to 99 (100 numbers)
+list = [i for i in range(100)]
+# randomly shuffle the list
+shuffle(list)
+print(list)
