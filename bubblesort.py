b700c3fdc0fd1229d43d184404b40c355337072f
Auwliya123 <spam.auwliya@gmail.com>
2021-12-15 10:01:47 +0100

    bubblesort added

---
+++ bubblesort.py
@@ -0,0 +1,26 @@
+import random
+import numberrandomizer
+import time
+
+def swapnumbers(list):
+    for index in range(len(list) - 1):
+        if list[index] > list[index + 1]:
+            list[index], list[index + 1] = list[index + 1], list[index]
+
+def ordercheck(list):
+    for index in range(len(list) - 1):
+        if list[index] > list[index + 1]:
+            return False
+    return True      
+
+def bubblesort(list):
+    while ordercheck(list) == False:
+        swapnumbers(list)
+
+list = numberrandomizer.randomizelist(1000)
+
+time_start = time.perf_counter()
+bubblesort(list)
+time_end = time.perf_counter()
+
+print(f"List: {list}, sorted in {time_end - time_start} seconds")
