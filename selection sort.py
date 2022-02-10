e44f86b4b6500bb4a75e390d40d12dcc18e9be91
Buildandplay <wiebevdmeij@gmail.com>
2022-02-10 12:16:57 +0100

    Added comments to Selection

--- selection sort.py
+++ selection sort.py
@@ -1,6 +1,10 @@
 import numberrandomizer
 import time
 
+#The code searches through the list of numbers. It starts with the smallest number and compares all numbers after that with the smallest one.
+#If there is one smaller, it compares all next numbers with that one. When it has found the smallest number and compared all numbers in the list,
+#it switches the smallest number to the first position in the list. The next smallest number goes next to this first smallest number and so on.
+#It loops until the list is sorted.
 def selectionsort(list):
     for i in range(len(list)):
         smallest=list[i]
@@ -15,10 +19,13 @@ def selectionsort(list):
 
     return list
 
-list = numberrandomizer.randomizelist(1000)
+#The list gets set to the randomized amount of numbers.
+list = numberrandomizer.randomizelist(10000)
 
+#Runs the script and uses a timer to time how long it takes the selectionsort to run.
 time_start = time.perf_counter()
 selectionsort(list)
 time_end = time.perf_counter()
 
+#Prints the list and time so you can see the sorted list and how long it took.
 print(f"List: {list}, sorted in {time_end - time_start} seconds")
